952104def5946f9a28c9bf6046bfa8d6df31318f
Илья Орлов <ilya.orlov@rktv.ru>
2025-05-28 12:47:16 +0500

    feat: add stub postgres, sqlalchemy, alembic to project

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
 from typing import Union
 
-from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from src.database import SessionLocal
+from src.models import Book
+
+from fastapi import FastAPI, Depends
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/books")
+async def read_books(db: Session = Depends(get_db)):
+    return db.query(Book).all()
